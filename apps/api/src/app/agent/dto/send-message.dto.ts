7a93c7ddded127a39d1e1a61fe21d2fcbcc6af77
Valentyn Yakymenko <vale.yakymenko@gmail.com>
2026-04-07 17:10:43 +0300

    Revert "Tmp"

--- apps/api/src/app/agent/dto/send-message.dto.ts
+++ apps/api/src/app/agent/dto/send-message.dto.ts
@@ -1,16 +1,5 @@
-import { IsArray, IsOptional, IsString } from 'class-validator';
-
 export class SendMessageDto {
-  @IsString()
   text!: string;
-
-  @IsOptional()
-  @IsArray()
-  @IsString({ each: true })
   images?: string[];
-
-  @IsOptional()
-  @IsArray()
-  @IsString({ each: true })
   attachmentFilenames?: string[];
 }
